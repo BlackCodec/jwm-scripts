2f820a9d8063950d842b47e05fd7134338294220
Black_Codec <7785428+BlackCodec@users.noreply.github.com>
2022-11-22 11:31:52 +0100

    Update 20221122.1129
    Add support for different mime folders and resolve issue with path and file that contains spaces in their name.

--- menu/dynamic-folder-shell.sh
+++ menu/dynamic-folder-shell.sh
@@ -21,11 +21,25 @@ function printEnd() {
 
 function getIconFile() {
   SRCFILE=$1
-  EXT=$(file --mime-type ${SRCFILE} | awk -F ":" '{print $2}' | awk -F "/" '{print $2}')
-  ICON=$(find ${CURRENT_ICON_THEME}/mimes/ -name "${EXT}.*" | head -n 1)
-  if [ -z "${ICON}" ]; then
-    EXT=$(file --mime-type ${SRCFILE} | awk -F ":" '{print $2}' | awk -F "/" '{print $1}' | sed "s| ||g")
-    ICON=$(find ${CURRENT_ICON_THEME}/mimes/ -name "${EXT}.*" | head -n 1)
+  MIMEDIR="${CURRENT_ICON_THEME}/mimes"
+  if [ ! -d "${MIMEDIR}" ]; then
+    MIMEDIR="${CURRENT_ICON_THEME}/mimetypes"
+    if [ ! -d "${MIMEDIR}" ]; then
+      MIMEDIR="${CURRENT_ICON_THEME}"
+    fi
+  fi
+  ICON=""
+  if [ -d "${MIMEDIR}" ]; then
+    EXT=$(file --mime-type ${SRCFILE} | awk -F ":" '{print $2}' | awk -F "/" '{print $2}')
+    ICON=$(find ${MIMEDIR}/ -name "${EXT}.*" | head -n 1)
+    if [ -z "${ICON}" ]; then
+      EXT=$(file --mime-type ${SRCFILE} | awk -F ":" '{print $2}' | sed "s| ||g" | sed "s|/|-|g")
+      ICON=$(find ${MIMEDIR}/ -name "${EXT}.*" | head -n 1)
+    fi
+    if [ -z "${ICON}" ]; then
+      EXT=$(file --mime-type ${SRCFILE} | awk -F ":" '{print $2}' | awk -F "/" '{print $1}' | sed "s| ||g")
+      ICON=$(find ${MIMEDIR}/ -name "${EXT}.*" | head -n 1)
+    fi
   fi
   echo "${ICON}"
 }
@@ -37,13 +51,13 @@ function getIconDir() {
 function printDir() {
   SRCDIR=$1
   DIRICO=$(getIconDir)
-  echo "<Program icon=\"${DIRICO}\" label=\"Open folder\">xdg-open ${FILEDIR}/</Program>"
+  echo "<Program icon=\"${DIRICO}\" label=\"Open folder\">xdg-open \"${FILEDIR}/\"</Program>"
   if [ $(find ${SRCDIR} -mindepth 1 -maxdepth 1 -type d | wc -l) -gt 0 ]; then
     printSep
     for THE_DIR_NAME in $(find ${SRCDIR} -mindepth 1 -maxdepth 1 -type d ); do
       THE_DIR_NAME=$(echo "${THE_DIR_NAME}" | sed "s|//|/|g")
       echo "<Dynamic icon=\"${DIRICO}\" label=\"$(basename ${THE_DIR_NAME})\">"
-      echo "exec:${EXEC_FILE_NAME} ${THE_DIR_NAME}"
+      echo "exec:${EXEC_FILE_NAME} \"${THE_DIR_NAME}\""
       echo "</Dynamic>"
     done
   fi
@@ -55,14 +69,17 @@ function printFile() {
     for THE_FILE_NAME in $(find ${FILEDIR} -maxdepth 1 -type f ); do
       THE_FILE_NAME=$(echo "${THE_FILE_NAME}" | sed "s|//|/|g")
       FILEICO=$(getIconFile ${THE_FILE_NAME})
-      echo "<Program icon=\"${FILEICO}\" label=\"$(basename ${THE_FILE_NAME})\">xdg-open ${THE_FILE_NAME}</Program>"
+      echo "<Program icon=\"${FILEICO}\" label=\"$(basename ${THE_FILE_NAME})\">xdg-open \"${THE_FILE_NAME}\"</Program>"
     done
   fi
 }
 
+BCK_IFS="${IFS}"
+IFS=$'\n'
 printStart
-printDir $CURRENT_DIR
+printDir ${CURRENT_DIR}
 printSep
-printFile $CURRENT_DIR
+printFile ${CURRENT_DIR}
 printEnd
-exit 0
+IFS="${BCK_IFS}"
+exit 1
